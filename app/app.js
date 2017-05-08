d5caf0133081d0e6fd344b1833ca3c7afae75279
sebastian-kopiczko <sebastian.kopiczko@gmail.com>
2017-05-08 18:26:51 +0200

    routing pages from menu

--- app/app.js
+++ app/app.js
@@ -1,8 +1,39 @@
-angular.module('workTimeApp', ['workTimeApp'])
-    .controller('mainCtrl', function ($scope) {
-        $scope.isPushed = true;
-        $scope.pushMenu = function ($scope) {
-            $scope.isPushed = !$scope.isPushed;
-            console.log('ok');
-        }
-    });
+var workTimeApp = angular.module('workTimeApp', ['ngRoute']);
+
+workTimeApp.controller('mainCtrl', function ($scope) {
+    $scope.isPushed = true;
+    $scope.msg = 'ttttt';
+    $scope.pushMenu = function ($scope) {
+        $scope.isPushed = !$scope.isPushed;
+        console.log('ok');
+    }
+});
+workTimeApp.controller('chatbotCtrl', function ($scope) {
+    $scope.msg = 'chatbot';
+});
+workTimeApp.controller('chartsCtrl', function ($scope) {
+    $scope.msg = 'charts';
+});
+workTimeApp.controller('helpCtrl', function ($scope) {
+    $scope.msg = 'help';
+});
+
+workTimeApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'templates/home.html',
+            controller: 'mainCtrl'
+        })
+        .when('/chatbot', {
+            templateUrl: 'templates/chatbot.html',
+            controller: 'chatbotCtrl'
+        })
+        .when('/charts', {
+            templateUrl: 'templates/charts.html',
+            controller: 'chartsCtrl'
+        })
+        .when('/help', {
+            templateUrl: 'templates/help.html',
+            controller: 'helpCtrl'
+        });
+});
